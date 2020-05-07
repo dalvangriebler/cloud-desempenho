d9e9dbac19b531103c2a29969e920da766f3066c
rickleon <ricardo.leonarczyk95@gmail.com>
2020-05-07 15:39:28 -0400

    add: adding the uperf benchmark

---
+++ benchmarks/uperf/config.h
@@ -0,0 +1,224 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Built on */
+#define BUILD_DATE "Thu May  7 19:26:12 UTC 2020"
+
+/* Configured on */
+#define CONFIGURED_ON "Thu May  7 19:26:12 UTC 2020"
+
+/* Debug */
+/* #undef DEBUG */
+
+/* collect nw stats */
+#define ENABLE_NETSTAT 1
+
+/* Define to 1 if you have the <alloca.h> header file. */
+#define HAVE_ALLOCA_H 1
+
+/* Define to 1 if you have the <atomic.h> header file. */
+/* #undef HAVE_ATOMIC_H */
+
+/* Have BSWAP_ macros */
+/* #undef HAVE_BSWAP */
+
+/* Define to 1 if you have the `clock_gettime' function. */
+#define HAVE_CLOCK_GETTIME 1
+
+/* Have libcpc */
+/* #undef HAVE_CPC */
+
+/* Define to 1 if you have the `gethrtime' function. */
+/* #undef HAVE_GETHRTIME */
+
+/* Define to 1 if you have the `gethrvtime' function. */
+/* #undef HAVE_GETHRVTIME */
+
+/* Define to 1 if the system has the type `hrtime_t'. */
+/* #undef HAVE_HRTIME_T */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Have libkstat */
+/* #undef HAVE_LIBKSTAT */
+
+/* Define to 1 if you have the `ssl' library (-lssl). */
+/* #undef HAVE_LIBSSL */
+
+/* Define to 1 if you have the <linux/unistd.h> header file. */
+#define HAVE_LINUX_UNISTD_H 1
+
+/* Define to 1 if you have the `mach_absolute_time' function. */
+/* #undef HAVE_MACH_ABSOLUTE_TIME */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `nanosleep' function. */
+#define HAVE_NANOSLEEP 1
+
+/* Define to 1 if you have the <poll.h> header file. */
+#define HAVE_POLL_H 1
+
+/* Define to 1 if you have the `pthread_self' function. */
+#define HAVE_PTHREAD_SELF 1
+
+/* Have rds */
+/* #undef HAVE_RDS */
+
+/* Have sctp */
+#define HAVE_SCTP 1
+
+/* Have sctp_sendmsg() */
+/* #undef HAVE_SCTP_SENDMSG */
+
+/* Have sctp_sendv() */
+/* #undef HAVE_SCTP_SENDV */
+
+/* Have sendfilev */
+/* #undef HAVE_SENDFILEV */
+
+/* Define to 1 if you have the <siginfo.h> header file. */
+/* #undef HAVE_SIGINFO_H */
+
+/* Define to 1 if you have the <signal.h> header file. */
+#define HAVE_SIGNAL_H 1
+
+/* Have ssl */
+/* #undef HAVE_SSL */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strlcat' function. */
+/* #undef HAVE_STRLCAT */
+
+/* Define to 1 if you have the `strlcpy' function. */
+/* #undef HAVE_STRLCPY */
+
+/* Define to 1 if you have the `strncat' function. */
+#define HAVE_STRNCAT 1
+
+/* Define to 1 if you have the `strncpy' function. */
+#define HAVE_STRNCPY 1
+
+/* Define to 1 if you have the <stropts.h> header file. */
+#define HAVE_STROPTS_H 1
+
+/* Define to 1 if you have the <sys/byteorder.h> header file. */
+/* #undef HAVE_SYS_BYTEORDER_H */
+
+/* Define to 1 if you have the <sys/int_limits.h> header file. */
+/* #undef HAVE_SYS_INT_LIMITS_H */
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <sys/lwp.h> header file. */
+/* #undef HAVE_SYS_LWP_H */
+
+/* Define to 1 if you have the <sys/poll.h> header file. */
+#define HAVE_SYS_POLL_H 1
+
+/* Define to 1 if you have the <sys/sendfile.h> header file. */
+#define HAVE_SYS_SENDFILE_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/ttycom.h> header file. */
+/* #undef HAVE_SYS_TTYCOM_H */
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <sys/uio.h> header file. */
+#define HAVE_SYS_UIO_H 1
+
+/* Define to 1 if you have the <sys/varargs.h> header file. */
+/* #undef HAVE_SYS_VARARGS_H */
+
+/* Define to 1 if you have the <termio.h> header file. */
+#define HAVE_TERMIO_H 1
+
+/* Have udp */
+#define HAVE_UDP 1
+
+/* Define to 1 if the system has the type `uint32_t'. */
+#define HAVE_UINT32_T 1
+
+/* Define to 1 if the system has the type `uint64_t'. */
+#define HAVE_UINT64_T 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <wait.h> header file. */
+#define HAVE_WAIT_H 1
+
+/* Define to 1 if you have the `_lwp_self' function. */
+/* #undef HAVE__LWP_SELF */
+
+/* Name of package */
+#define PACKAGE "uperf"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "realneel@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "uperf"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "uperf 1.0.7"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "uperf"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0.7"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Use only threads */
+/* #undef STRAND_THREAD_ONLY */
+
+/* Setting machine os to Darwin */
+/* #undef UPERF_DARWIN */
+
+/* Setting machine os to FreeBSD */
+/* #undef UPERF_FREEBSD */
+
+/* Setting machine os to Linux */
+#define UPERF_LINUX 1
+
+/* Setting machine os to Solaris */
+/* #undef UPERF_SOLARIS */
+
+/* Have cpu perf counters? */
+/* #undef USE_CPC */
+
+/* Have version1 cpu perf counters? */
+/* #undef USE_CPCv1 */
+
+/* Have version2 cpu perf counters? */
+/* #undef USE_CPCv2 */
+
+/* Have getrusage */
+#define USE_GETRUSAGE 1
+
+/* Version number of package */
+#define VERSION "1.0.7"
